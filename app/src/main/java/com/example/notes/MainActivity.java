a23299b7729e6da6ca9f9884b73c7282002dfe0b
Eng-Abdallah Gamal <abdallahgamal5500@gmail.com>
2020-03-23 07:41:44 +0200

    The first project (notes)

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ServerValue;
@@ -41,9 +45,6 @@ private Map<String, String> date_time;
         dialog_text=view.findViewById(R.id.dialog_text);
         dialog_btn_add=view.findViewById(R.id.dialog_btn_add);
         dialog_btn_cancel=view.findViewById(R.id.dialog_btn_cancel);
-        dialog_title_value=dialog_title.getText().toString();
-        dialog_text_value=dialog_text.getText().toString();
-
         builder.setView(view);
         builder.setCancelable(false);
         final AlertDialog dialog = builder.create();
@@ -57,16 +58,14 @@ private Map<String, String> date_time;
         dialog_btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (dialog_text_value.isEmpty() || dialog_title_value.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
-                } else {
-                    FirebaseDatabase database = FirebaseDatabase.getInstance();
-                    DatabaseReference myRef = database.getReference("Notes");
-                    id = myRef.push().getKey();
-                    date_time = ServerValue.TIMESTAMP;
-                    Note_data note_data = new Note_data(dialog_title_value,dialog_text_value,id,date_time);
-                    myRef.child(id).setValue(note_data);
-                }
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+                dialog_title_value=dialog_title.getText().toString();
+                dialog_text_value=dialog_text.getText().toString();
+                date_time = ServerValue.TIMESTAMP;
+                id = myRef.push().getKey();
+                Note_data note_data = new Note_data(dialog_title_value, dialog_text_value,id,date_time);
+                myRef.child(id).setValue(note_data);
             }
         });
         dialog.show();
--- app/src/main/java/com/example/notes/Note_data.java
+++ app/src/main/java/com/example/notes/Note_data.java
@@ -4,13 +4,47 @@ import java.util.Map;
 
 public class Note_data {
 
-    private String Title,Text,Id;
-    private Map<String, String> Time;
+    private String title;
+    private String text;
+    private String id;
+    private Map<String, String> time;
 
     public Note_data(String title, String text, String id, Map<String, String> time) {
-        Title = title;
-        Text = text;
-        Id = id;
-        Time = time;
+        this.title = title;
+        this.text = text;
+        this.id = id;
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Map<String, String> getTime() {
+        return time;
+    }
+
+    public void setTime(Map<String, String> time) {
+        this.time = time;
     }
 }
